301a64d2017458b13af422e40b7474ea24230ed2
dieter <dieterkrause31960@gmail.com>
2023-06-22 18:02:59 +0200

    Bearbeitung Beispiel12
    Signed-off-by: dieter <dieterkrause31960@gmail.com>

--- src/example/Beispiel12.js
+++ src/example/Beispiel12.js
@@ -23,15 +23,28 @@ const { Draht } = require(path.resolve(
  *  l = "3 km", d = "0.9 mm", R = "84.5 ohm"
  */
 function Beispiel12 (input) {
+  // Speicherort für Daten
   const jsonfile = path.resolve('src/json/example/beispiel12.json')
 
-  const Kennzeichnung = {}
-  const Parameter = {
-    l: input.l,
-    d: input.d,
-    R: input.R
+  // JSON Vorlage Kennzeichnung
+  const Kennzeichnung = dbJson.readJSONFile(
+    path.resolve('src/json/kennzeichnung.json')
+  )
+
+  const Parameter = input
+
+  // TODO:  Hier wird das Aussehen des W1 bestimmt
+  // Die Koordinaten sind hier vieleicht noch zu früh
+  // z.B. Schutzleiter, Außenleiter, Nulleiter  ...
+  const Visual = {
+    // vis: 'vW', // Funktionsname zum Zeichnen des Betriebsmittel
+    // id: 'W1',  ist Kennzeichnung
+    opacity: 0.25,
+    x: 50,
+    y: 50,
+    height: 5,
+    width: 600
   }
-  const Visual = {}
   const W1 = new Draht(Kennzeichnung, Parameter, Visual)
 
   const PK = new PlanemetrieKernel()
@@ -44,6 +57,7 @@ function Beispiel12 (input) {
   let ρ = EK.ρRAl()
 
   let erg = {
+    Betriebsmittel: W1,
     Parameter: {
       l: input.l,
       d: input.d,
@@ -57,10 +71,10 @@ function Beispiel12 (input) {
   dbJson.writeJSONItem(jsonfile, erg)
   return erg
 }
-// let input = {
-//   l: '3 km',
-//   d: '0.9 mm',
-//   R: '84.5 ohm'
-// }
-// console.log(Beispiel12(input))
-exports.func = Beispiel12
+let input = {
+  l: '3 km',
+  d: '0.9 mm',
+  R: '84.5 ohm'
+}
+console.log(Beispiel12(input))
+// exports.func = Beispiel12
