7e457f9c9e3f2a4a6d1813370d000585c1047add
dieter <dieterkrause31960@gmail.com>
2023-09-02 19:17:46 +0200

    Aufgabe16
    Signed-off-by: dieter <dieterkrause31960@gmail.com>

--- src/d3/lib/Betriebsmittel/vR.mjs
+++ src/d3/lib/Betriebsmittel/vR.mjs
@@ -1,37 +1,37 @@
-function vR (data, pos) {
-  const R = d3.create('svg:g')
-  R.selectAll('g')
+function vR(data, pos) {
+  const R = d3.create("svg:g");
+  R.selectAll("g")
     .data(data)
     .enter()
-    .append('rect')
-    .attr('fill', d => {
-      return d.color
+    .append("rect")
+    .attr("fill", (d) => {
+      return d.color;
     })
-    .attr('fill-opacity', d => {
-      return d.fillopacity
+    .attr("fill-opacity", (d) => {
+      return d.fillopacity;
     })
-    .attr('style', d => {
-      return d.style
+    .attr("style", (d) => {
+      return d.style;
     })
-    .attr('x', pos[0])
-    .attr('y', pos[1])
-    .attr('height', d => {
-      return d.height
+    .attr("x", pos[0])
+    .attr("y", pos[1])
+    .attr("height", (d) => {
+      return d.height;
     })
-    .attr('width', d => {
-      return d.width
-    })
-  R.selectAll('text')
+    .attr("width", (d) => {
+      return d.width;
+    });
+  R.selectAll("text")
     .data(data)
     .enter()
-    .append('text')
-    .attr('x', pos[0])
-    .attr('y', pos[1] - 5)
-    .text(d => {
-      return d.text
-    })
+    .append("text")
+    .attr("x", pos[0])
+    .attr("y", pos[1] - 5)
+    .text((d) => {
+      return d.text;
+    });
 
-  return R.node()
+  return R.node();
 }
 
-export { vR }
+export { vR };
--- src/example/Uebung19.js
+++ src/example/Uebung19.js
@@ -1,9 +1,9 @@
-require('use-strict')
-const path = require('path')
-const dbJson = require(path.resolve('controllers/dbJson.js'))
-const { ElektroKernel, Kaltleiter } = require(path.resolve('include/system'))
+require("use-strict");
+const path = require("path");
+const dbJson = require(path.resolve("controllers/dbJson.js"));
+const { ElektroKernel, Kaltleiter } = require(path.resolve("include/system"));
 
-const { readKonstante } = require(path.resolve('src/js/readKonstante.js'))
+const { readKonstante } = require(path.resolve("src/js/readKonstante.js"));
 /**
  * @function Uebung19
  *
@@ -11,34 +11,34 @@ const { readKonstante } = require(path.resolve('src/js/readKonstante.js'))
  * von 40 kohm. Wie groß ist der Temperaturkoeffizient, wenn bei 25.5°C der
  * Widerstand 41 kohm beträgt.
  */
-function Uebung19 (input) {
-  const jsonfile = path.resolve('src/json/example/uebung19.json')
+function Uebung19(input) {
+  const jsonfile = path.resolve("src/json/example/uebung19.json");
 
   // const δ20 = dbJson.readJSONFile(path.resolve('src/json/konstanten.json'))
   //   .konstanten.Vergleichstemperatur.wert
   const Kennzeichnung = dbJson.readJSONFile(
-    path.resolve('src/json/kennzeichnung.json')
-  )
+    path.resolve("src/json/kennzeichnung.json")
+  );
 
-  const Parameter = input
-  let R1 = new Kaltleiter(Kennzeichnung, Parameter, {})
-  R1.Parameter.δ20 = readKonstante('Vergleichstemperatur').toString()
+  const Parameter = input;
+  let R1 = new Kaltleiter(Kennzeichnung, Parameter, {});
+  R1.Parameter.δ20 = readKonstante("Vergleichstemperatur").toString();
 
-  const EK = new ElektroKernel()
+  const EK = new ElektroKernel();
   EK.parameter({
     R20: R1.Parameter.R20,
     Rδ2: R1.Parameter.Rδ2,
     δ2: R1.Parameter.δ2,
-    δ20: R1.Parameter.δ20
-  })
-  R1.Parameter.Rα20 = EK.α20().toString()
+    δ20: R1.Parameter.δ20,
+  });
+  R1.Parameter.Rα20 = EK.α20().toString();
 
-  dbJson.writeJSONItem(jsonfile, R1)
+  dbJson.writeJSONItem(jsonfile, R1);
 }
-const input = {
-  R20: '40 kohm',
-  Rδ2: '41 kohm',
-  δ2: '25.5 celsius'
-}
-Uebung19(input)
-// exports.func = Uebung19
+// const input = {
+//   R20: '40 kohm',
+//   Rδ2: '41 kohm',
+//   δ2: '25.5 celsius'
+// }
+// Uebung19(input)
+exports.func = Uebung19;
